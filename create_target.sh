23788b931c2b2365efe75e8428a8f07a1c18a44e
Shekar Tippur <shekartippur@shekars-mbp.attlocal.net>
2017-10-23 17:52:23 -0700

    Correcting README

--- create_target.sh
+++ create_target.sh
@@ -3,7 +3,7 @@ python3 -m venv target
 pwd=`pwd`
 rm -f ${pwd}/swagger_server.zip
 source target/bin/activate
-cp src/requirements.txt target
+cp src/python/flask/requirements.txt target
 rm -rf temp
 swagger-codegen generate -i swaggerglobal.yml -l python-flask -o temp
 for files in `ls temp/swagger_server/models/*.py|egrep -v "__init__|binary.py"`; do 
@@ -12,16 +12,15 @@ for files in `ls temp/swagger_server/models/*.py|egrep -v "__init__|binary.py"`;
   if [ ! -f $dest ]; then
      cp $src $dest
   fi
-  cp temp/swagger_server/swagger/swagger.yaml src/swagger_server/swagger/swagger.yaml
-  cp temp/swagger_server/controllers/default_controller.py src/swagger_server/controllers
+  cp temp/swagger_server/swagger/swagger.yaml src/python/flask/swagger_server/swagger/swagger.yaml
+  cp temp/swagger_server/controllers/default_controller.py src/python/flask/swagger_server/controllers
 done
-cp -r src/swagger_server target
+cp -r src/python/flask/swagger_server target
 python curate.py ## Generates controller code
 cp /tmp/stage/* target/swagger_server/controllers
 cd target
 pip install -r requirements.txt
 cd lib/python3.6/site-packages
-exit
 zip -r9 ${pwd}/swagger_server.zip *
 cd $pwd/target
 zip -g -r ${pwd}/swagger_server.zip swagger_server
