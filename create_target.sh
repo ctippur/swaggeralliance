829a9a8018df8ad331457827ca653927dc6c1373
Shekar Tippur <ShekarTippur@fico.com>
2018-03-24 10:56:09 -0700

    Adding bunch of bug fixes for generating controller stubs

--- create_target.sh
+++ create_target.sh
@@ -1,4 +1,4 @@
-rm -rf stage temp 
+rm -rf stage temp
 deactivate 2>/dev/null
 pwd=`pwd`
 src='src/python/flask/'
@@ -12,37 +12,42 @@ if [ ! -d "target" ]; then
 fi
 source target/bin/activate
 
-## 
+##
 mkdir -p stage
 cp ${src}/requirements.txt stage 2>/dev/null
 
 # Generate new code under temp
 swagger-codegen generate -i swaggerglobal.yml -l python-flask -o temp
 mkdir -p ${src}/swagger_server/swagger
+mkdir -p ${src}/swagger_server/controllers
 cp temp/swagger_server/swagger/swagger.yaml ${src}/swagger_server/swagger/swagger.yaml
 cp temp/swagger_server/controllers/*_controller.py ${src}/swagger_server/controllers
 
 ## Code to copy mopdel code if it does not exist
-for files in `ls temp/swagger_server/models/*.py|egrep -v "__init__|binary.py|config.py"`; do 
+for files in `ls temp/swagger_server/models/*.py|egrep -v "__init__|binary.py|config.py"`; do
   modelsrc=$files
-  dest="${src}/`basename $files`"
+  dest="${src}/swagger_server/models/`basename $files`"
+  echo "Copying $modelsrc $dest"
   #dest=`echo $files|sed "s|temp|${src}|g"`
   if [ ! -f $dest ]; then
      cp $modelsrc $dest
   fi
 done
 
-python curate.py ## Generates controller code under /tmp/stage
+python curate.py ## Generates controller code under stage
 
-cp /tmp/stage/*_controller.py ${src}/swagger_server/controllers
+cp stage/*_controller.py ${src}/swagger_server/controllers
+#for files in `ls /tmp/stage/*_controller.py`; do
+#  cp $files ${src}/swagger_server/controllers
+#done
 ## Code to copy controller code if it does not exist
-for files in `ls /tmp/stage/*.py|egrep -v "__init__"`; do
+for files in `ls stage/*.py|egrep -v "__init__"`; do
   srccontroller=$files
-  #dest=`echo $files|sed 's/\/tmp\/stage/src\/swagger_server\/controllers/g'`
-  dest="${src}/`basename $files`"
+  echo "Copying $srccontroller $dest"
+  dest="${src}/swagger_server/controllers/`basename $files`"
   #echo $dest
   if [ ! -f $dest ]; then
-     cp $src $dest
+     cp $srccontroller $dest
   fi
 done
 cp -r ${src}/swagger_server stage
@@ -50,8 +55,8 @@ cp -r ${src}/swagger_server stage
 pip install -r ${pwd}/stage/requirements.txt
 cp -r target/lib/python3.6/site-packages/* stage
 cp ${src}/zappa_settings.json stage
-
 exit
+
 zip -r9 ${pwd}/swagger_server.zip * -x \*.pyc\*
 cd $pwd/target
 zip -g -r ${pwd}/swagger_server.zip swagger_server -x \*.pyc\*
--- curate.py
+++ curate.py
@@ -12,7 +12,7 @@ folder='temp/swagger_server/controllers'
 src='src/python/flask/'
 
 ## Temporaru staging area
-folderstg='/tmp/stage'
+folderstg='stage'
 
 ## Create a stage folder
 try:
@@ -23,7 +23,7 @@ except:
 default_folders=glob.glob(folder + '/*controller.py')
 print (default_folders)
 #sys.exit(0)
-regexstr=['_post','_get', '_put', 'delete']
+regexstr=['post','get' ]
 
 ## Import string
 importStr="""\
@@ -32,14 +32,12 @@ import connexion
 
 ## Post string
 postStrFooter="""\
-    if connexion.request.is_json:
         body = Payload.from_dict(connexion.request.get_json())
     return 'do some magic!'
 """
 
 ## Footer string
 getStrFooter="""\
-    return 'do some magic!'
 """
 
 ## Get string
@@ -77,8 +75,10 @@ for model in modelList:
 ## operation - _get / _post
 ## line - function (def)
 def createStub(controllerfile,operation,line):
-    print (controllerfile, operation, line)
+    #print (controllerfile, operation, line)
     if (re.match(operation, '_post')):
+        print ("Writing line " + line + "to file " + controllerfile)
+
         if os.path.exists(controllerfile):
             with open(controllerfile, "a") as w:
                 w.write('\n')
@@ -105,11 +105,22 @@ def createStub(controllerfile,operation,line):
 
 
 def ret_filename(origfile):
+    #print (origfile)
+    args=''
+    retstr=''
+    str=''
     for str in regexstr:
-        print (origfile, str)
+        #print (origfile, str)
+
         if re.search(str,origfile):
             retstr=origfile.split(str)[0].replace('def ','')
-            return (retstr,str)
+            if retstr =='':
+                retstr=origfile.split('(')[0].replace('def ','')
+                args=origfile.split('(')[1].split(')')[0]
+    if retstr == '':
+        retstr=origfile.split('(')[0].replace('def ','')
+        args=origfile.split('(')[1].split(')')[0]
+    return (retstr,str,args)
 
 functionCall=None
 
@@ -119,14 +130,20 @@ os.system(cmd)
 
 for default_controller in default_folders:
     #default_controller=folder + '/default_controller.py'
+    print ("Default controller " + default_controller)
     default_controller_file=default_controller.split('/')[-1]
     # /tmp/stage/temp/swagger_server/controllers/store_controller.py
+    print ("Writing to " + folderstg + '/' + default_controller_file)
     fStgDefaultController = open(folderstg + '/' + default_controller_file, 'w')
 
     ## Read default controller that was generated
     with open(default_controller) as infile:
+        defn=0
+        post=0
+        defline=''
         ## Read each line
         for line in infile:
+
             ## See if it matches operationid
             if (re.match(r'operationId', line)):
                 l=re.findall(r"(\w)(get|post|put)\(", line)[0]
@@ -135,6 +152,8 @@ for default_controller in default_folders:
                     line=re.sub(r'((get|post|put|delete))', r'_\1', line)
             ## Match the start of a function
             if (re.match(r'^def', line)):
+                defn=1
+                defline=line
                 #print (ret_filename(line))
                 # Each line beginning def is a
                 ## Different types of function definitions
@@ -142,7 +161,7 @@ for default_controller in default_folders:
                 # 2. binary_branch_post(body, branch)
                 # 3. clinic_city_city_get(city):
                 # 4. clinic_get():
-    
+
                 ## This is to handle mangled def with get|post. Example, idget instead of id_get
                 l=[]
                 try:
@@ -152,49 +171,118 @@ for default_controller in default_folders:
                         line=re.sub(r'((get|post|put))', r'_\1', line)
                 except:
                     l=[]
-                print ("Line is " + line)
-                newfile,operation=ret_filename(line)
+                #print ("Line is " + line)
+                try:
+                    newfile,operation,args=ret_filename(line)
+                    #print (line,newfile,operation,args)
+                except:
+                    print (sys.exc_info())
+                    continue
                 #print ("Operation is " + operation)
-    
+
                 ## Get Arguments to the function
                 result=line.partition('(')[-1].rpartition(')')[0]
                 ## Get the function name
                 impFn=line.split('(')[0].replace('def ', '')
-    
+
                 ## Construct calling new function
                 ##from swagger_server.controllers.users import user_users_post
-    
                 if operation == '_post':
-                    importlocal="        from swagger_server.controllers." + newfile + " import " + impFn + "\n"
-                    #functionCall="        return(" + impFn + "(" + result + "))\n"
-                    functionCall="        res=" + impFn + "(" + result + ")\n" +  resLogicPost
-                elif operation == '_get':
                     importlocal="    from swagger_server.controllers." + newfile + " import " + impFn + "\n"
+                    #functionCall="        return(" + impFn + "(" + result + "))\n"
+                    functionCall="    res=" + impFn + "(" + result + ")\n" +  resLogicPost
+                else:
+                    operation = '_get'
+                    importlocal="from swagger_server.controllers." + newfile + " import " + impFn + "\n"
                     #functionCall="    return(" + impFn + "(" + result + "))\n"
-                    functionCall="    res=" + impFn + "(" + result + ")\n" + resLogicGet
-    
+                    functionCall="res=" + impFn + "(" + result + ")\n" + resLogicGet
+
+                ## New logic
+                ## if connexion.request.is_json
+
                 #importlocal="        from swagger_server.controllers." + newfile + " import " + impFn + "\n"
                 #functionCall="        " + impFn + "(" + result + ")\n"
-    
+
                 ## Controller file
                 controllerfile=folderstg + '/' + newfile + '.py'
-    
+                """
                 ## Check if new file exists
                 if os.path.exists(controllerfile):
                     ## Check if operation exists
                     if line in open(controllerfile).read():
-                        print("controller file " + controllerfile + " exists and no changes for " + line)
+                        print("controller file Here " + controllerfile + " exists and no changes for " + line)
                     else:
+                        print ("Writing line " + line + "to file " + controllerfile)
                         ## Create stub
                         createStub(controllerfile,operation, line)
                 else:
                     ## Create file and stub
                     createStub(controllerfile,operation, line)
+                """
                 fStgDefaultController.write(line)
+
             #elif (re.match(r'return \'do some magic!\'',line)):
+            ## new Only POST has request.is_json in it
+            elif ('if connexion.request.is_json' in line):
+                post=1
+                ## THis means it is post
+                importlocal="        from swagger_server.controllers." + newfile + " import " + impFn + "\n"
+                #functionCall="        return(" + impFn + "(" + result + "))\n"
+                functionCall="        res=" + impFn + "(" + result + ")\n" +  resLogicPost
+                controllerfile=folderstg + '/' + newfile + '.py'
+                operation="_post"
+                newline=defline + '\n' + line
+                ## Check if new file exists
+                if os.path.exists(controllerfile):
+                    ## Check if operation exists
+                    if line in open(controllerfile).read():
+                        print("controller file " + controllerfile + " exists and no changes for " + line)
+
+                    else:
+                        ## Create stub
+                        createStub(controllerfile,operation, newline)
+                else:
+                    ## Create file and stub
+                    createStub(controllerfile,operation, newline)
+                fStgDefaultController.write(line)
+
             elif ('return \'do some magic!\'' in line):
-                fStgDefaultController.write(importlocal)
-                fStgDefaultController.write(functionCall)
+                if post == 0:
+                    importlocal="    from swagger_server.controllers." + newfile + " import " + impFn + "\n"
+                    #functionCall="    return(" + impFn + "(" + result + "))\n"
+                    functionCall="    res=" + impFn + "(" + result + ")\n" + resLogicGet
+                    ## Controller file
+                    controllerfile=folderstg + '/' + newfile + '.py'
+                    operation='_get'
+
+                    newline=defline + '\n' + line
+
+                    ## Check if new file exists
+                    if os.path.exists(controllerfile):
+                        ## Check if operation exists
+
+                        if line in open(controllerfile).read():
+                            print("controller file here1 " + controllerfile + " exists and no changes for " + newline)
+                        else:
+                            print ("post " + str(post), " def " + str(defn), "defline " + newline)
+
+                            ## Create stub
+                            createStub(controllerfile,operation, newline)
+                    else:
+                        ## Create file and stub
+                        print ("Writing line " + line + "to file " + controllerfile)
+                        createStub(controllerfile,operation, newline)
+                    fStgDefaultController.write(line)
+
+                try:
+                    fStgDefaultController.write(importlocal)
+                    fStgDefaultController.write(functionCall)
+                except:
+                    fStgDefaultController.write(line)
+                defn=0
+                post=0
             else:
                 fStgDefaultController.write(line)
+                #defn=0
+                #post=0
 fStgDefaultController.close()
